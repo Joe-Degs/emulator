9cfe8d915a76943fb243d69e754a93053f7e0ff3
Joe-Degs <jpedegs8990@gmail.com>
2021-09-07 23:03:05 +0100

    make dirty block size tuneable

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ const (
 	PERM_WRITE      = 1 << 1 // write permission
 	PERM_EXEC       = 1 << 2 // executable permission
 	PERM_RAW        = 1 << 3 // read-after-write permission
+
+	DIRTY_BLOCK_SIZE = 0x7f
 )
 
 // VirtAddr is a guest virtual address
@@ -32,8 +34,8 @@ type VirtAddr uint
 // confuse me as the other mechanisms of keeping track of blocks
 // so its cool
 
-// Block is a block of memory. and the block of memory is the map of the start
-// of the block to the end of the blocks that have been modified.
+// Block is a block of memory, it maps the start of the block to the end
+// of the block
 type Block = map[VirtAddr]VirtAddr
 
 // Mmu is an isolated memory space
@@ -42,7 +44,7 @@ type Mmu struct {
 	memory []uint8
 	// access restrictions on individual locations in memory
 	permissions []Perm
-	// map of 16-byte aligned blocks of memory that have been modified
+	// map of modified blocks of memory
 	dirty Block
 	// tracks the current allocation
 	curAlloc VirtAddr
@@ -53,16 +55,16 @@ func NewMmu(size uint) *Mmu {
 		memory:      make([]uint8, size),
 		permissions: make([]Perm, size),
 		dirty:       make(Block),
-		curAlloc:    VirtAddr(0x38),
+		curAlloc:    VirtAddr(0x800),
 	}
 }
 
 // Reset restores all memory back to the original state. This allows us to
 // create one emulator and fork it to run multiple things.
 func (m *Mmu) Reset(other *Mmu) {
-	for addr, size := range m.dirty {
+	for addr, endAddr := range m.dirty {
 		start := int(addr)
-		end := start + int(size)
+		end := int(endAddr)
 
 		// restore memory state
 		copy(m.memory[start:end], other.memory[start:end])
@@ -142,7 +144,7 @@ func (m *Mmu) WriteFrom(addr VirtAddr, buf []uint8) int {
 	// copy the slice `buf` into memory pointed to by `addr`
 	n := copy(m.memory[int(addr):len(buf)+int(addr)], buf)
 
-	// update RAW permissions and mark the written portion of memory as dirty
+	// update permissions and allow reading after writing
 	if hasRAW {
 		for i, p := range perms {
 			if (p & PERM_RAW) != 0 {
@@ -151,16 +153,18 @@ func (m *Mmu) WriteFrom(addr VirtAddr, buf []uint8) int {
 		}
 	}
 
-	// 16-byte aligned block to keep track of modified memory
-	blockStart := (int(addr) + 0xf) &^ 0xf
+	// aligned block to keep track of modified memory
+	blockStart := (int(addr) + DIRTY_BLOCK_SIZE) &^ DIRTY_BLOCK_SIZE
+	round := DIRTY_BLOCK_SIZE + 1
 	if blockStart > int(addr) {
-		blockStart -= 16
+		blockStart -= round
 	}
-	// number of 16-byte blocks that have been modified
-	numBlocks := int((n+0xf)&^0xf) / 16
-
+	// align block to the dirty block size
+	numBlocks := int((n+DIRTY_BLOCK_SIZE)&^DIRTY_BLOCK_SIZE) / round
+	// end index of the aligned block
+	blockEnd := VirtAddr(blockStart + (numBlocks * round) - 1)
 	// add block `start - end` to dirty blocks map
-	m.dirty[VirtAddr(blockStart)] = VirtAddr(blockStart + (numBlocks * 0x10) - 1)
+	m.dirty[VirtAddr(blockStart)] = blockEnd
 	return n
 }
 
@@ -175,7 +179,7 @@ func (m *Mmu) ReadInto(addr VirtAddr, buf []uint8) (n int) {
 	for _, p := range perms {
 		// check if any part of the memory has is read-after-write
 		hasRAW = hasRAW || ((p & PERM_RAW) != 0)
-		// check if all perms on region of memory is read
+		// check if all perms on region of memory is read perm
 		if (p & PERM_READ) == 0 {
 			return 0
 		}
@@ -183,9 +187,9 @@ func (m *Mmu) ReadInto(addr VirtAddr, buf []uint8) (n int) {
 
 	if hasRAW {
 		// checking if block of memory we are reading from is dirtied
-		alignedAddr := (int(addr) + 0xf) &^ 0xf
+		alignedAddr := (int(addr) + DIRTY_BLOCK_SIZE) &^ DIRTY_BLOCK_SIZE
 		if alignedAddr > int(addr) {
-			alignedAddr -= 16
+			alignedAddr -= (DIRTY_BLOCK_SIZE + 1)
 		}
 
 		_, ok := m.dirty[VirtAddr(alignedAddr)]
@@ -211,7 +215,7 @@ func (e Emulator) Fork() *Emulator {
 }
 
 func main() {
-	emu := NewEmulator(1024)
+	emu := NewEmulator(0x1000)
 	tmp := emu.Allocate(4)
 	emu.WriteFrom(tmp, []byte("asdf"))
 
@@ -227,7 +231,7 @@ func main() {
 		if n == 0 {
 			panic(n)
 		}
-		fmt.Println(string(buf))
+		fmt.Printf("%x\n", buf)
 		forked.Reset(emu.Mmu)
 		spew.Dump(forked)
 	}(emu.Fork())
